9e8dc79f814b0b914142b841231941a93d95b320
vanshika704 <sharmavanshi704@gmail.com>
2024-11-01 21:09:07 +0530

    mm

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Genie',
    debugShowCheckedModeBanner: false,
    
       getPages: [
